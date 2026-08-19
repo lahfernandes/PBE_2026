73aaa52eb8677e99adbcaaad8c832e7321dc2803
LAVINIA FERNANDES DE LIMA <lavinia.lima@portalsesisp.org.br>
2026-08-19 15:41:33 -0300

    15:40 19/08

---
+++ 02_funcao/exemplo.php
@@ -0,0 +1,25 @@
+<?php
+
+$frequencia = 90;
+$media1 = 9;
+
+echo "Lavínia - ";
+if($frequencia < 75) { // Frequência insuficiente
+    echo "Reprovado por falta";
+
+}
+elseif ($media1 >= 7){ // Maior que 7 aprovado
+    echo "Aprovado";
+
+}
+elseif ($media1 >= 5){ // Nota: entre 5 e 6.9 = recuperação
+    echo "Recuperação";
+
+}
+else{ // Media insuficiente reprovado
+    echo "Reprovado";
+
+}
+?>
+
+
