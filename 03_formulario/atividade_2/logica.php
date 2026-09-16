7df598156abfcc95846c65257e6a1f4e0676737d
LAVINIA FERNANDES DE LIMA <lavinia.lima@portalsesisp.org.br>
2026-09-16 10:51:50 -0300

    10:51 16/09

---
+++ 03_formulario/atividade_2/logica.php
@@ -0,0 +1,11 @@
+<?php
+$numero = $_POST['numero'];
+$dobro = $numero * 2;
+$triplo = $numero * 3;
+$quadrado = $numero * $numero;
+echo "O numero digitato foi: $numero<br>";
+echo "O dobro digitato foi: $dobro<br>";
+echo "O triplo digitato foi: $triplo<br>";
+echo "O quadrado digitato foi: $quadrado<br>";
+
+?>
