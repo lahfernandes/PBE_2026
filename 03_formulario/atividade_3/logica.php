3919d445d745d58b4e602f224ea52ed13ec608c0
LAVINIA FERNANDES DE LIMA <lavinia.lima@portalsesisp.org.br>
2026-09-16 15:54:39 -0300

    15:54 16/09

---
+++ 03_formulario/atividade_3/logica.php
@@ -0,0 +1,9 @@
+<?php
+$numero = $_POST['numero'];
+$multiplicação = $numero * 2;
+$triplo = $numero * 3;
+$quadrado = $numero * $numero;
+echo "O numero digitato foi: $numero<br>";
+echo "O dobro digitato foi: $dobro<br>";
+echo "O triplo digitato foi: $triplo<br>";
+echo "O quadrado digitato foi: $quadrado<br>";
---
+++ 03_formulario/atividade_5/logica.php
@@ -0,0 +1,11 @@
+<?php
+$nome = $_POST['nome'];
+$peso_em_kg = $_POST['peso_em_kg'];
+$altura_em_metros = $_POST['altura_em_metros'];
+
+$altura_em_metros = $altura_em_metros * 100;
+
+$imc = ($altura_em_metros + $altura_em_metros)/$peso_em_kg;
+
+require_once "view_relatorio.php";
+?>
